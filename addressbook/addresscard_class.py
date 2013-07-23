cbfe4ad8071117f77923cf6474f50d6fb76a227d
Justin <justin.poh@students.olin.edu>
2013-07-23 13:01:41 +0800

    Complete addresscard functionality complete

--- addressbook/addresscard_class.py
+++ addressbook/addresscard_class.py
@@ -30,3 +30,54 @@ class addresscard:
 		self.actualcard.append(fieldholder)
 
 		print self.actualcard[-1].fieldname, 'has been created with value:', self.actualcard[-1].fieldvalue
+
+	def deletefield(self, userspecname):
+		
+		foundindex=self.fieldsearcher(userspecname)
+
+		if foundindex[0]==True:
+
+			del self.actualcard[foundindex[1]]
+			print 'Entry successfully removed'
+			print ' '
+
+		elif foundindex[1]==False:
+
+			print 'Sorry. Entry not found'
+			print ' '
+
+	def editfield(self, userspecname, userspecvalue):
+
+		foundindex=self.fieldsearcher(userspecname)
+
+		if foundindex[0]==True:
+			self.actualcard[foundindex[1]].fieldvalue=userspecvalue
+
+			print self.actualcard[foundindex[1]].fieldname, 'has been changed. It is now:', self.actualcard[foundindex[1]].fieldvalue
+
+		elif foundindex[0]==False:
+
+			print 'Sorry. Entry not found'
+
+	def renamefield(self, userspecoldname, userspecnewname):
+
+		foundindex=self.fieldsearcher(userspecoldname)
+
+		if foundindex[0]==True:
+			self.actualcard[foundindex[1]].fieldname=userspecnewname
+
+			print self.actualcard[foundindex[1]].fieldname, 'has been changed.'
+
+		elif foundindex[0]==False:
+			print 'Sorry. Entry not found'
+
+	def fieldsearcher(self, userspecname):
+		indexcounter=0
+
+		for item in self.actualcard:
+			searchtest=(item.fieldname.lower() == userspecname.lower())
+
+			if searchtest==True:
+				return [searchtest, indexcounter]
+			else:
+				indexcounter +=1
